29ae9cd91e1ad8863b2ca8cd0680f709e849f474
Sidolux <midas78@gmail.com>
2018-09-15 09:37:21 +0200

    v1.5 Komentarze, formatowanie

--- functions.py
+++ functions.py
@@ -5,8 +5,9 @@ def player_marker(tekst=''):
     '''
     Returns selected Tic Tac Toe mark. Checks if input is o or x (CI)
 
+    :param tekst:
+    :return: str - marker
     '''
-
     char = ''
     select = ''
     while select.lower() not in ('x', 'o'):
@@ -17,6 +18,14 @@ def player_marker(tekst=''):
 
 
 def display_board(board):
+    '''
+    Draws a tic tac toe board
+
+    :param board:
+    :return:
+    '''
+
+
     print(f' {board[7]}|{board[8]}|{board[9]}')
     print(f' -----')
     print(f' {board[4]}|{board[5]}|{board[6]}')
@@ -25,10 +34,23 @@ def display_board(board):
 
 
 def place_marker(board, marker, position):
+    '''
+    Puts marker on board, without any checks
+    :param board: list, Table with board
+    :param marker: str, Marker to put
+    :param position: int, Position where to put
+    :return:
+    '''
     board[position] = marker
 
 
 def player_position(tekst=''):
+    '''
+    Asks for position on board
+
+    :param tekst: str, Text to display in question
+    :return:
+    '''
     position = None
     while position not in range(1, 10):
         usr_input = input(tekst)
@@ -45,21 +67,19 @@ def player_position(tekst=''):
 
 
 def check_win(board, marker):
+    '''
+    Checks win condition
+
+    :param board: table, Table with board
+    :param marker: str, Marker to check againsts
+    :return: bool, True - Selected marker win
+    '''
     win_conditions = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7))
     for win_cond in win_conditions:
         win_set = set()
         for win_pos in win_cond:
-#            print(str(win_pos)+board[win_pos], end='')
             win_set.add(board[win_pos])
-#        print('')
-#        print(win_cond, win_set, len(win_set), marker)
         if len(win_set)==1 and marker in win_set:
             return True
     return False
-
-    #if len(win_set) == 1 and win_set[0]==marker:
-    #    return True
-    #else:
-    #    return False
-
 
--- tictactoe.py
+++ tictactoe.py
@@ -1,15 +1,17 @@
 from os import system
-from sys import exit
+# from sys import exit
 from functions import *
 
 restart = True
+# Game restart loop
 while restart:
+    # setting variables
     xo = ['O', 'X']
     board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     who = 0
     pos = 0
-
     system('cls')
+    # display splash screen
     print('''
 -------------------------
 | Gra w kółko i krzyżyk |
@@ -21,6 +23,7 @@ Pola na planszy są ponumerowane jak na klawiaturze numerycznej komputera.
 Powodzenia.
 
     ''')
+    # ask for players
     player1_name = input('Podaj imię gracza 1: ')
     player2_name = input('Podaj imię gracza 2: ')
 
@@ -36,23 +39,29 @@ Powodzenia.
     print('')
     display_board(board)
     print('')
+    # main game loop
     while True:
+        # ask for position and check if place is available
         while board[pos] in ('X', 'O', '#'):
             pos = player_position(players[who] + ', gdzie postawić ' + xo[who] + ' (1-9)')
             if board[pos] != ' ':
                 print('Pole zajęte, wybierz inne pole.')
+        # put marker on selected position
         place_marker(board, xo[who], pos)
         system('cls')
         print('')
         display_board(board)
         print('')
-#        print(check_win(board, xo[who]))
+        # check if player won, or board full (tie)
         won = check_win(board, xo[who])
         if won or ' ' not in set(board):
+            # won message
             if won:
                 print(f'BRAWO {players[who]}!!! ZWYCIĘSTWO.')
+            # tie message
             else:
                 print('REMIS')
+            # ask if replay
             player_continue = ''
             while player_continue not in ('t', 'n'):
                 player_continue = input('\nGramy jeszcze raz T/N? ').lower()
